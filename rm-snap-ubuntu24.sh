a0ccfab3820fa913efa1ab76a008f68a1dd3302d
Gerardo Barcenas <gbarcenasjr@gmail.com>
2024-07-14 11:34:03 -0700

    Correct manager from apt to snap

--- rm-snap-ubuntu24.sh
+++ rm-snap-ubuntu24.sh
@@ -3,16 +3,21 @@
 #Variables
 confirm=""
 
-printf "Welcome to rm-snap-ubuntu24 by Gerardo Barcenas (ver 0.1)"
+printf "Welcome to rm-snap-ubuntu24 by Gerardo Barcenas (ver 1.0)"
 printf "This script will attempt toremove the following: \nfirefox \nthunderbird \ngtk-common-themes \ngnome-42-2204 \nsnapd-desktop-integration \nsnap-store \nfirmware-updater \nbare \ncore22 \nsnapd \n\n"
 
 # shellcheck disable=SC2162
 read -p "Do you want to continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
-sudo apt remove firefox gtk-common-themes gnome-42-2204 snapd-desktop-integration snap-store firmware-updater bare
-
+sudo snap remove firefox
+sudo snap remove gtk-common-themes
+sudo snap remove gnome-42-2204
+sudo snap remove snapd-desktop-integration
+sudo snap remove snap-store
+sudo snap remove firmware-updater
+sudo snap remove bare
+sudo snap remove thunderbird
 sudo snap remove core22
-
 sudo snap remove snapd
 
 confirm=""
@@ -28,21 +33,14 @@ do
 done
 
 sudo systemctl stop snapd
-
 sudo systemctl disable snapd
-
 sudo systemctl mask snapd
-
 sudo apt purge snapd -y
-
 sudo apt-mark hold snapd
 
 sudo rm -rf ~/snap
-
 sudo rm -rf /snap
-
 sudo rm -rf /var/snap
-
 sudo rm -rf /var/lib/snapd
 
 sudo tee -a nosnap.pref > /etc/apt/preferences.d <<EOT
